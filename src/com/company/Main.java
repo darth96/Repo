e76bd00e27cc4cebf7374370c296d9cb1c72f091
darth96 <malenkovm@gmail.com>
2021-02-23 20:32:55 -0500

    hey

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	byte numOfHrs = 24;
+        System.out.println(" There are " +  numOfHrs  +  " hours in a day " );
+    short numOfDays = 365;
+    short currentYear = 2021;
+        System.out.println(" There are " + numOfDays + " days in " + currentYear);
+    byte technoSoft = 50;
+        System.out.println(" There are " + technoSoft + " employees at TechnoSoft " );
+    int totalPopulation = 331000000;
+        System.out.println(" There are " + totalPopulation + " people in the US ");
+    float interestRate = 0.50f;
+    char percent = '%';
+        System.out.println(" Ally's savings account offers a rate of " + interestRate + percent );
+    int totalBalance = 500000;
+    char dollarSign = '$';
+        System.out.println(" Ally's checking account has a total balance of " + dollarSign + totalBalance );
+    boolean isSunRise = false;
+        System.out.println(" Does the sun rise from the west? " + isSunRise );
+    char firstInitial = 'M';
+    char secondInitial = 'M';
+        System.out.println( firstInitial + " First initial " + "Second initial " + secondInitial );
+
+
+    }
+}
