dd2880829fce4149e78f305dd8eb8137c8220d78
darth96 <malenkovm@gmail.com>
2021-03-22 23:24:46 -0400

    hey

---
+++ src/com/company/Homework8.java
@@ -0,0 +1,36 @@
+package com.company;
+
+
+import java.util.Arrays;
+
+public class Homework8 {
+    public static void main(String[] args) {
+        double[] doubleArray = {11.23,+ 43.3, +34, +65.76, +87};
+        double total= doubleArray[0]+ doubleArray[1] + doubleArray[2] + doubleArray[3]+ doubleArray[4];
+        System.out.println(total);
+
+
+
+
+
+        boolean isPrime =true;
+        int num =10;
+            if (num / 1== num && num / num ==1 && num!=0)
+                System.out.println(isPrime);
+            else if(num % 1!= num && num % num  !=1)
+        System.out.println("this isnt a prime number " +num);
+
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
