406e4c386dd61a91cb4f97c047d1b9dfce4ba4c3
darth96 <malenkovm@gmail.com>
2021-03-22 23:29:23 -0400

    hey

--- src/com/company/Homework8.java
+++ src/com/company/Homework8.java
@@ -5,20 +5,33 @@ import java.util.Arrays;
 
 public class Homework8 {
     public static void main(String[] args) {
-        double[] doubleArray = {11.23,+ 43.3, +34, +65.76, +87};
-        double total= doubleArray[0]+ doubleArray[1] + doubleArray[2] + doubleArray[3]+ doubleArray[4];
+        //Reverse String
+        String word ="racecaR";
+        String reverseWord= "";
+        String[] wordArray =word.split("");
+        for (int i=wordArray.length-1; i>=0; i--)
+            reverseWord= reverseWord + wordArray[i];
+        System.out.println("reverseWord= " + reverseWord);
+        boolean isPalindrome=true;
+        if (reverseWord.equalsIgnoreCase(word))
+            System.out.println(isPalindrome);
+        else
+            System.out.println("this isnt palindrome");
+
+        //Add array numbers
+        double [] doubleArray = {11.23, 43.3, 34, 65.76, 87};
+        double total = doubleArray[0]+ doubleArray[1]+ doubleArray[2]+ doubleArray[3]+doubleArray[4];
         System.out.println(total);
 
+        //Prime number
+        boolean isPrime= true;
+        int num= 11;
+        // for (int i=2 ;i<num; i++)
+        if (num%1==num && num%num==num)
+            System.out.println(isPrime);
+        else
+            System.out.println("This isnt prime");
 
-
-
-
-        boolean isPrime =true;
-        int num =10;
-            if (num / 1== num && num / num ==1 && num!=0)
-                System.out.println(isPrime);
-            else if(num % 1!= num && num % num  !=1)
-        System.out.println("this isnt a prime number " +num);
 
         }
 
